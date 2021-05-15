1b80c573e0366960cb468c36bf7df5ed65cffd14
Aman Sethi <amansethi221306@gmail.com>
2021-05-16 01:00:45 +0530

    tried webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import App from "./App";
-
+// import Logo
+import Logo from "./logo.svg";
+import _ from "lodash";
+// inside innerHTML
 const root = document.getElementById("root");
 root.innerHTML = `
+<img src="${Logo}" alt="build icon" />;
 <h1> learning web tooling today </h1>
-${App({ name: "Tanay" })}
+${App({name: "Tanay"})}
 `;
